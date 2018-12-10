063c0279d2aa4a580176896ca9e08c366cf8fdb6
netbuffer <javawiki@163.com>
2018-12-10 11:08:27 +0800

    init

---
+++ src/main/java/cn/netbuffer/App.java
@@ -0,0 +1,16 @@
+package cn.netbuffer;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+/**
+ * @author admin
+ * @date 2018/12/10
+ */
+public class App {
+    public static void main(String[] args) {
+        List names = Lists.newArrayList();
+        System.out.println("Hello World:" + names);
+    }
+}
